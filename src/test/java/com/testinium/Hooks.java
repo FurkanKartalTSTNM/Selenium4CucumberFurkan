de0fcdef94aa0bea2db16c06bbcb64c1d1ef6881
Enes Turan <mehmet.aksahin@testinium.com>
2022-11-11 13:17:49 +0300

    hooks

--- src/test/java/com/testinium/Hooks.java
+++ src/test/java/com/testinium/Hooks.java
@@ -8,10 +8,15 @@ import java.util.Map;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -21,55 +26,82 @@ import java.util.concurrent.TimeUnit;
 public class Hooks {
 
     public String URL = "http://127.0.0.1:4444/wd/hub";
-    public static String browserName = "chrome";
-
     protected static WebDriver driver;
-    ChromeOptions chromeOptions;
+    protected static Actions actions;
+
     DesiredCapabilities capabilities;
+    ChromeOptions chromeOptions;
+
+    FirefoxOptions firefoxOptions;
+
     private static final String hubCloudDev = "https://hubclouddev.testinium.com/wd/hub";
     private static final String hubLocal = "http://localhost:4444/wd/hub";
     private static final String hubCloudProd = "https://hubclouddev.testinium.com/wd/hub";
 
+    String browserName = "chrome";
+    String selectPlatform = "win";
+
     @Before
     public void beforeTest() throws MalformedURLException {
-        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        try {
+            if (StringUtils.isEmpty(System.getenv("key"))) {
 
-        final String key = System.getenv("key");
-        final String testID = System.getenv("testID");
+                if ("win".equalsIgnoreCase(selectPlatform)) {
+                    if ("chrome".equalsIgnoreCase(browserName)) {
+                        driver = new ChromeDriver(chromeOptions());
+                        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+                    } else if ("firefox".equalsIgnoreCase(browserName)) {
+                        driver = new FirefoxDriver(firefoxOptions());
+                        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+                    }
+                } else if ("mac".equalsIgnoreCase(selectPlatform)) {
+                    if ("chrome".equalsIgnoreCase(browserName)) {
+                        driver = new ChromeDriver(chromeOptions());
+                        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+                    } else if ("firefox".equalsIgnoreCase(browserName)) {
+                        driver = new FirefoxDriver(firefoxOptions());
+                        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+                    }
+                    actions = new Actions(driver);
+                }
 
-        if (StringUtils.isNotEmpty(key)) {
-            // testinium tarafından başlatıldığında
-                System.out.println("Testiniumda ayaga kalkti");
-                dc = DesiredCapabilities.chrome();
-                dc.setCapability("testinium:testID", testID);
-                dc.setCapability("testinium:takesScreenshot", true);
-                dc.setCapability("testinium:recordsVideo", true);
-                // Cloud USERNAME:ACCESS_KEY
-                dc.setCapability("key", System.getProperty("key"));
-                driver = new RemoteWebDriver(new URL(hubCloudProd),dc);
-
-        } else {
-            // test local'den testinium'suz başlatıldığında
-            /*
-            System.setProperty("webdriver.chrome.webdriver", "/Users/ebubekir/Dev/data/chromedriver");
-            webdriver = new ChromeDriver();
-
-             */
-
-            System.out.println("Localde ayaga kalkti");
-            driver = new ChromeDriver(chromeOptions());
-            driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-
-            //System.setProperty("webdriver.gecko.driver", "/Users/ebubekir/Dev/data/geckodriver");
-/*
-            driver = new RemoteWebDriver(
-                    new URL("http://localhost:4444/wd/hub"), dc
-            );
-
- */
-
-
+            } else {
+                System.out.println("Testiniumda ayaga kalkacak");
+                ChromeOptions options = new ChromeOptions();
+                capabilities = DesiredCapabilities.chrome();
+                options.setExperimentalOption("w3c", false);
+                options.addArguments("disable-translate");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--start-fullscreen");
+                Map<String, Object> prefs = new HashMap<>();
+                options.setExperimentalOption("prefs", prefs);
+                capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+                capabilities.setCapability("key", System.getenv("key"));
+                browserName = System.getenv("browser");
+                driver = new RemoteWebDriver(new URL("https://hubclouddev.testinium.com/wd/hub"), capabilities);
+                actions = new Actions(driver);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
+
+    }
+
+    public FirefoxOptions firefoxOptions() {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        capabilities = DesiredCapabilities.firefox();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        firefoxOptions.addArguments("--kiosk");
+        firefoxOptions.addArguments("--disable-notifications");
+        firefoxOptions.addArguments("--start-fullscreen");
+        FirefoxProfile profile = new FirefoxProfile();
+        capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+        capabilities.setCapability("marionette", true);
+        firefoxOptions.merge(capabilities);
+        System.setProperty("webdriver.gecko.driver", "web_driver/geckodriver");
+        return firefoxOptions;
+
     }
 
     @After
